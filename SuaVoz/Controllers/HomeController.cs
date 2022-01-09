ed880bc86089555b68e58aafe99714624ffdcb4c
Abimael Lovera <abimaellovera@gmail.com>
2022-01-09 01:51:09 -0300

    Controllers Home About Rights Login

--- SuaVoz/Controllers/HomeController.cs
+++ SuaVoz/Controllers/HomeController.cs
@@ -10,59 +10,67 @@ using System.Threading.Tasks;
 
 namespace SuaVoz.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        public IActionResult Termos()
-        {
-            return View();
-        }
-
-        public IActionResult Denuncia()
-        {
-            return View();
-        }
-
-        public IActionResult FaixaEtaria()
-        {
-            return View();
-        }
-
-        public IActionResult Genero()
-        {
-            return View();
-        }
-
-        public IActionResult Regiao()
-        {
-            return View();
-        }
-
-        public IActionResult TipoViolencia()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
     }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+    public IActionResult About()
+    {
+      return View();
+    }
+
+    public IActionResult Rights()
+    {
+      return View();
+    }
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    public IActionResult Termos()
+    {
+      return View();
+    }
+
+    public IActionResult Denuncia()
+    {
+      return View();
+    }
+
+    public IActionResult FaixaEtaria()
+    {
+      return View();
+    }
+
+    public IActionResult Genero()
+    {
+      return View();
+    }
+
+    public IActionResult Regiao()
+    {
+      return View();
+    }
+
+    public IActionResult TipoViolencia()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
